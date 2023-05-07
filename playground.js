1b5fb09489fe34033dec5a7a87b99db43a3552c9
Mikaella Margarette Dichoso <ap.mikaellamargarette.dichoso@apecschools.edu.ph>
2023-05-07 11:14:23 +0000

    initial commit

--- playground.js
+++ playground.js
@@ -1,15 +1,46 @@
-/* 
-// Metacrafters - Beginning Javascript
-//
-// This is a javascript playground for testing your javascript code!
-// When you are ready to test, simply right click and select "Run Code"
-// to see the result print below. If you have more then one snippet of code,
-// you can highlight the code you want to test, and then right click and select "Run Code"
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
 */
 
-// Enter your code below this line
+// create a variable to hold your NFT's
+const NFTs = []
 
-// Code example
-for(var i=1; i<=5; i++) {
- console.log(i);
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT(_name, _faveColor, _motto, _faveDessert) {
+   const NFT = {
+      "name": _name,
+      "faveColor": _faveColor,
+      "motto": _motto,
+      "faveDessert": _faveDessert
+   }
+   NFTs.push(NFT);
+   console.log("Minted:" + _name)
 }
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs() {
+   for (let i = 0; i < NFTs.length; i++) (
+      console.log(NFTs[i])
+   )
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+   console.log(NFTs.length)
+}
+
+// call your functions below this line
+mintNFT("Mikaella", "Blue", "YOLO", "Cake");
+listNFTs();
+getTotalSupply();
+
+
